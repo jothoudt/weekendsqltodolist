7254dc3f2b45d57493abcf9dd0bf3e36c1d717b1
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-03-19 22:13:21 -0500

    GET route working

--- server/modules/routes/list.router.js
+++ server/modules/routes/list.router.js
@@ -14,4 +14,15 @@ router.post('/', (req, res) => {
     })
 })
 
+router.get('/', (req, res) =>{
+    let queryText=`SELECT * FROM "toDo" ORDER BY "id";`;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+    .catch(error =>{
+        console.log('error getting list', error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,7 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('in JQ');
+    displayTasks();
     $('#submitTaskButton').on('click', submitTask);
 };
 
@@ -22,3 +23,23 @@ function submitTask(){
         alert('unable to add task')
     });
 };
+
+function displayTasks(){
+    $.ajax({
+        method: 'GET',
+        url: '/listofthings'
+    }).then(function(response){
+        console.log('response from server', response);
+        for(let i=0; i<response.length; i++ ){
+            $('#outputDiv').append(`
+            <tr>
+            <td>${response[i].task}</td>
+            <td><button data-id=${response[i].id} class="completeTaskButton">Complete</button></td>
+            <td><button data-id=${response[i].id} class="deleteTaskButton">X</button</td>
+            </tr>
+            `);
+        }
+    }).catch(function(error){
+        console.log('error in GET', error);
+    })
+}
