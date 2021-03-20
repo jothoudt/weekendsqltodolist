f5bc6051f32d85c6748f4e4a703619a340f822d0
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-03-20 12:32:03 -0500

    add more comments and clean up format of code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,14 +6,86 @@ function readyNow(){
     $('#submitTaskButton').on('click', submitTask);
     $('#taskOutput').on('click', '.completeTaskButton', completeTask);
     $('#taskOutput').on('click', '.deleteTaskButton', deleteTask);
-};
+};//end readyNow
+
+function completeTask(){
+    console.log('in completeTask');
+    let completeId= $(this).data('id');
+    $(this).addClass('green')
+    //ajax put
+    $.ajax({
+        method: 'PUT',
+        url: '/listofthings/' + completeId
+    }).then(function(response){
+        console.log('back from PUT:', response);
+        displayTasks();
+    }).catch(function(err){
+        console.log(err);
+        alert('did not update');
+    })//end ajax call
+}//end completeTask
+
+function deleteTask(){
+    console.log('in delete task');
+    let taskId= $(this).data('id');
+    //ajax DELETE call
+    $.ajax({
+        method: 'DELETE',
+        url: '/listofthings/' + taskId,
+    }).then(function(response){
+        console.log('back from Delete', response)
+        displayTasks();
+    }).catch(function(response){
+        console.log(err);
+        alert('did not delete')
+    })//end ajax delete
+}//end deleteTask
+
+function displayTasks(){
+    //ajax GET call
+    $.ajax({
+        method: 'GET',
+        url: '/listofthings'
+    }).then(function(response){
+        //empty before appending
+        $('#taskOutput').empty();
+        console.log('response from server', response);
+        //loop through tasks
+        for(let i=0; i<response.length; i++ ){
+            //variable to display complete button or check mark
+            let completedHTML=`<button data-id=${response[i].id} class="completeTaskButton">Complete</button>`
+            //variable to set the class of tr
+            let classCss=`<tr class="white">`
+            //variable to number each task
+            let count=  Number([i])+1
+            console.log(count);
+            let thClass=`<th>`
+            if(response[i].completed === true){
+            completedHTML=`<span class="check">&#10003;</span>`;
+             classCss= `<tr class="green">`
+             thClass= `<th class="crossedOut">`
+            }//end if
+            $('#taskOutput').append(`
+            ${classCss}
+            ${thClass}${count}. ${response[i].task}</th>
+            <th>${completedHTML}</th>
+            <button data-id=${response[i].id} class="completeTaskButton">Complete</button>
+            <th><button data-id=${response[i].id} class="deleteTaskButton">X</button></th>
+            </tr>
+            `);//end append
+        }//end for
+    }).catch(function(error){
+        console.log('error in GET', error);
+    })//end ajax
+}//end displayTasks
 
 function submitTask(){
+    //create a variable to send to server
     let newTask={
         task:$('#taskToDO').val(),
         completed: false
     }
-
+    //ajax POST
     $.ajax({
         method: 'POST',
         url: '/listofthings',
@@ -24,68 +96,5 @@ function submitTask(){
     }).catch(function(error){
         console.log('error in POST', error);
         alert('unable to add task')
-    });
-};
-
-function displayTasks(){
-    $.ajax({
-        method: 'GET',
-        url: '/listofthings'
-    }).then(function(response){
-        $('#taskOutput').empty();
-        console.log('response from server', response);
-        for(let i=0; i<response.length; i++ ){
-            let completedHTML=`<button data-id=${response[i].id} class="completeTaskButton">Complete</button>`
-            let classCss=`<tr class="white">`
-            let count=  Number([i])+1
-            console.log(count);
-            let thClass=`<th>`
-            if(response[i].completed === true){
-            completedHTML=`<span class="check">&#10003;</span>`;
-             classCss= `<tr class="green">`
-             thClass= `<th class="crossedOut">`
-            }
-            $('#taskOutput').append(`
-            ${classCss}
-            ${thClass}${count}. ${response[i].task}</th>
-            <th>${completedHTML}</th>
-            <button data-id=${response[i].id} class="completeTaskButton">Complete</button>
-            <th><button data-id=${response[i].id} class="deleteTaskButton">X</button></th>
-            </tr>
-            `);
-        }
-    }).catch(function(error){
-        console.log('error in GET', error);
-    })
-}
-
-function deleteTask(){
-    console.log('in delete task');
-    let taskId= $(this).data('id');
-    $.ajax({
-        method: 'DELETE',
-        url: '/listofthings/' + taskId,
-    }).then(function(response){
-        console.log('back from Delete', response)
-        displayTasks();
-    }).catch(function(response){
-        console.log(err);
-        alert('did not delete')
-    })
-}
-
-function completeTask(){
-    console.log('in completeTask');
-    let completeId= $(this).data('id');
-    $(this).addClass('green')
-    $.ajax({
-        method: 'PUT',
-        url: '/listofthings/' + completeId
-    }).then(function(response){
-        console.log('back from PUT:', response);
-        displayTasks();
-    }).catch(function(err){
-        console.log(err);
-        alert('did not update');
-    })
-}
+    });//end ajax
+};//end submitTask
