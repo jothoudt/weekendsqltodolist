694773d86d143d11e1add4c88d411d015ebd32f5
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-03-22 22:58:20 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express =require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router= require('./modules/routes/list.router');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
